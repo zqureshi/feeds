1085d594920557d0e565ba87b41fc675a19227ac
Zeeshan Qureshi <zee@zqureshi.in>
2017-03-26 23:02:58 -0400

    Convert CounterTest to AssertJ

--- src/test/java/in/zqureshi/feeds/api/CounterTest.java
+++ src/test/java/in/zqureshi/feeds/api/CounterTest.java
@@ -2,13 +2,16 @@ package in.zqureshi.feeds.api;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.*;
 
 public class CounterTest {
     @Test
     public void equality() {
-        assertEquals(new Counter("/foo", 1L), new Counter("/foo", 1L));
-        assertNotEquals(new Counter("/bar", 1L), new Counter("/bar", 100L));
+        assertThat(new Counter("/foo", 1l))
+            .isEqualTo(new Counter("/foo", 1l));
+
+        assertThat(new Counter("/bar", 1l))
+            .isNotEqualTo(new Counter("/bar", 100l));
     }
 
     @Test
@@ -16,8 +19,8 @@ public class CounterTest {
         Counter c1 = new Counter("/foo", 1L);
         Counter c2 = new Counter("/foo", 2L);
 
-        assertTrue(c1.compareTo(c2) < 0);
-        assertTrue(c2.compareTo(c1) > 0);
-        assertTrue(c1.compareTo(c1) == 0);
+        assertThat(c1.compareTo(c2)).isLessThan(0);
+        assertThat(c2.compareTo(c1)).isGreaterThan(0);
+        assertThat(c1.compareTo(c1)).isZero();
     }
 }
